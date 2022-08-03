a482bb066bd3c8065805bb1855e5ac050f21ca31
danispinxo <genericpronoun@gmail.com>
2022-08-03 22:56:45 +0000

    functional page fetcher; no optional stretch cases

--- fetcher.js
+++ fetcher.js
@@ -5,38 +5,33 @@ const fs = require('fs');
 let pageToFetch = userInput[0];
 let fileToCreate = userInput[1];
 
-console.log(pageToFetch);
-console.log(fileToCreate);
-
 
 const pageFetcher = (pageToFetch) => {
-  console.log(pageToFetch);
-  
+
   request(`${pageToFetch}`, (error, response, body) => {
-    let requestError = error;
+    if (error) {
+      console.log(error);
+    }
     let status = response;
-    // console.log(body); 
-    return body
+    
+    const fileWriter = (fileToCreate) => {
+
+      fs.writeFile(`${fileToCreate}`, body, err => {
+      
+        if (err) {
+          console.error(err);
+        }
+
+        if(!err) {
+          console.log(`Downloaded and saved ${body.length} bytes to ${fileToCreate}`);
+        }
+
+      });
+      
+    };
+
+    fileWriter(fileToCreate);
   });
 };
 
-const fileWriter = (fileToCreate) => {
-  console.log(fileToCreate);
-
-  content = pageFetcher(pageToFetch);
-
-  fs.writeFile(`${fileToCreate}`, content, err => {
-  
-  if (err) {
-    console.error(err);
-  }
-
-  if(!err) {
-    console.log(`File written successfully!`);
-  }
-
-  });
-  
-}
-
-fileWriter(fileToCreate);
+pageFetcher(pageToFetch);
